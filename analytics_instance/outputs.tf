5431fd6dc8f699a892605031ff9e3b326b38a4b2
dev-null-loop <dev.null.loop@gmail.com>
2025-03-30 17:59:31 +0300

    analytics:+service_url output

--- analytics_instance/outputs.tf
+++ analytics_instance/outputs.tf
@@ -1,4 +1,9 @@
 output "id" {
-  description = "The resource OCID."
+  description = "The resource OCID"
   value       = oci_analytics_analytics_instance.this.id
+}
+
+output "service_url" {
+  description = "URL of the Analytics service"
+  value       = oci_analytics_analytics_instance.this.service_url
 }
